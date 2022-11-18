6e6ce493eae60b4f78ab7005b98b13c4e6d34a18
Matthew Oley <64291507+mroley1@users.noreply.github.com>
2022-11-18 10:28:38 -0500

    stuff idfk

--- js/index.js
+++ js/index.js
@@ -4,26 +4,34 @@ function zoomin() {
     let comp = getComputedStyle(document.querySelector("#index_bg_image")).backgroundSize
     if (comp == "100%") {
         var value = 0
+        var op = "+"
     }
     else {
-        var value = parseInt(comp.match(/(?<=\+ )-?\d+/)[0])
+        var value = parseInt(comp.match(/(?<=[\+-] )\d+/)[0])
+        var op = comp.match(/[\+-]/)[0]
     }
-    if (value >= 1000) {return}
-    var new_val = value + 100
-    console.log(100, value, 100 + value, comp)
-    document.getElementById("index_bg_image").style.backgroundSize = "calc(100% + " + new_val + "px)"
+    console.log(op, comp, value)
+    if (op == "-"){value = 0-value}
+    else {value = 0+value}
+    var new_val = value + 200
+    if (new_val >= 1600) {return}
+    document.getElementById("index_bg_image").style.backgroundSize = "calc(100% " + op + " " + Math.abs(new_val) + "px)"
 }
 
 function zoomout() {
     let comp = getComputedStyle(document.querySelector("#index_bg_image")).backgroundSize
     if (comp == "100%") {
         var value = 0
+        var op = "-"
     }
     else {
-        var value = parseInt(comp.match(/(?<=\+ )-?\d+/)[0])
+        var value = parseInt(comp.match(/(?<=[\+-] )\d+/)[0])
+        var op = comp.match(/[\+-]/)[0]
     }
-    if (value <= -1000) {return}
-    var new_val = value - 100
-    console.log(100, value, 100 - value, comp)
-    document.getElementById("index_bg_image").style.backgroundSize = "calc(100% + " + new_val + "px)"
+    console.log(op, comp, value)
+    if (op == "-"){value = 0-value}
+    else {value = 0+value}
+    var new_val = value - 200
+    if (new_val <= -100) {return}
+    document.getElementById("index_bg_image").style.backgroundSize = "calc(100% " + op + " " + Math.abs(new_val) + "px)"
 }
